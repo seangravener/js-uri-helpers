857b16469d74b33a2ab8244982a7bc9535ad59b1
Sean Gravener <sean@gravener.net>
2013-12-08 09:32:43 -0500

    wip

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 
+ console.log('-- app.js --');
+
+ var qs = $('.has-qs').prop('src');
+
+    console.log(qs);
+    console.log( url.qs('func1', qs) );
+
+    console.log( url.qs('loc') );
+
+    // loop through all script tags
+
+});
