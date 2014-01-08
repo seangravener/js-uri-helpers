7017fb8a79df144c85a2ebaf7782b8b6392ac5a0
Sean Gravener <sean@gravener.net>
2014-01-08 14:05:40 -0500

    add helpers wrapper

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
 
   console.log('-- qs in <script> tag (app.js)--');
   var uri = $('script#app-js').prop('src');
-  console.log('uri: ' + uri);
-  console.log('a  : ' + url.qs('a', uri) );
-  console.log('b  : ' + url.qs('b', uri) );
-  console.log('c  : ' + url.qs('c', uri) );
-  console.log('d  : ' + url.qs('d', uri) );
-  console.log('e  : ' + url.qs('e', uri) ); // not in qs
+  console.log('uri: ' + helpers.uri);
+  console.log('a  : ' + helpers.url.qs('a', uri) );
+  console.log('b  : ' + helpers.url.qs('b', uri) );
+  console.log('c  : ' + helpers.url.qs('c', uri) );
+  console.log('d  : ' + helpers.url.qs('d', uri) );
+  console.log('e  : ' + helpers.url.qs('e', uri) ); // not in qs
 
   console.log('-- qs in page URI (app.js) --');
-  console.log('uri: ' + url.seg('uri') );
-  console.log('a  : ' + url.qs('a') );
-  console.log('b  : ' + url.qs('b') );
-  console.log('c  : ' + url.qs('c') );
-  console.log('d  : ' + url.qs('d') );
-  console.log('e  : ' + url.qs('e') );
+  console.log('uri: ' + helpers.url.seg('uri') );
+  console.log('a  : ' + helpers.url.qs('a') );
+  console.log('b  : ' + helpers.url.qs('b') );
+  console.log('c  : ' + helpers.url.qs('c') );
+  console.log('d  : ' + helpers.url.qs('d') );
+  console.log('e  : ' + helpers.url.qs('e') );
 
   console.log('--------------------------------');
 
--- url-helpers.js
+++ url-helpers.js
@@ -1,75 +1,83 @@
 /*
  * URL Helpers
+ * Sean Gravener
  */
 
-var url = {
+var helpers = (function (helpers) {
 
-  /*
-   * Gets a uri segment or an entire uri
-   * 
-   * Example
-   * URI: /these/are/segments/
-   
-     var name = url.seg( 3 ); // name == 'segments'
-     var uri  = url.seg( 'uri' ); // uri == '/these/are/segments'
+  var url = {
+
+    /*
+     * Gets a uri segment or an entire uri
+     * 
+     * Example
+     * URI: /these/are/segments/
      
-   */
-  seg: function ( segment ) {
+       var name = url.seg( 3 ); // name == 'segments'
+       var uri  = url.seg( 'uri' ); // uri == '/these/are/segments'
+       
+     */
+    seg: function ( segment ) {
 
-    var loc       = window.location,
-        url       = loc.protocol + "//" + loc.host + loc.pathname;
-        segs      = window.location.pathname.split( '/' ),
-        uri       = window.location.href.replace( loc.protocol + "//" + loc.host, '' );
+      var loc       = window.location,
+          url       = loc.protocol + "//" + loc.host + loc.pathname;
+          segs      = window.location.pathname.split( '/' ),
+          uri       = window.location.href.replace( loc.protocol + "//" + loc.host, '' );
 
-    // just return the entire uri path
-    if ( segment == 'uri' ) {
-      return uri;
-    }
+      // just return the entire uri path
+      if ( segment == 'uri' ) {
+        return uri;
+      }
 
-    else if ( segs.length >= segment ) {
-      return segs[ segment ];
-    }
+      else if ( segs.length >= segment ) {
+        return segs[ segment ];
+      }
 
-    else {
-      return false;
-    }
+      else {
+        return false;
+      }
 
-  },
-  
-  /*
-   * Gets a query string value from the current url or a custom string
-   * 
-   * Example
-   * URI: /these/are/segments/?cat=chester&dog=wilbur
-   
-     var cat = url.qs( 'cat' ); // cat == 'chester'
-     var dog = url.qs( 'dog' ); // dog == 'wilbur'
-     var pig = url.qs( 'pig', '?pig="humphry"' ); // pig == 'humphry'
+    },
+    
+    /*
+     * Gets a query string value from the current url or a custom string
+     * 
+     * Example
+     * URI: /these/are/segments/?cat=chester&dog=wilbur
      
-   */
-  qs: function ( key, qs ) {
-  
-    var qs     = qs || window.location.href,
-        keys   = [], 
-        val,
-        vals   = qs.slice( qs.indexOf('?') + 1 ).split(/[;&]/);
+       var cat = url.qs( 'cat' ); // cat == 'chester'
+       var dog = url.qs( 'dog' ); // dog == 'wilbur'
+       var pig = url.qs( 'pig', '?pig="humphry"' ); // pig == 'humphry'
+       
+     */
+    qs: function ( key, qs ) {
+    
+      var qs     = qs || window.location.href,
+          keys   = [], 
+          val,
+          vals   = qs.slice( qs.indexOf('?') + 1 ).split(/[;&]/);
 
-    for ( var i = 0; i < vals.length; i++ ) {
+      for ( var i = 0; i < vals.length; i++ ) {
+        
+        val = vals[i].split('=');
+        
+        // [ "key", "value" ]
+        keys.push( val[0] );
+        
+        // key = value
+        keys[ val[0] ] = val[1];
+
+      }
       
-      val = vals[i].split('=');
+      var value = ( keys[key] ) || false;
       
-      // [ "key", "value" ]
-      keys.push( val[0] );
-      
-      // key = value
-      keys[ val[0] ] = val[1];
+      return value;
 
     }
-    
-    var value = ( keys[key] ) || false;
-    
-    return value;
+  };
 
+  return {
+    url: url
   }
 
-}
+})(helpers || {});
